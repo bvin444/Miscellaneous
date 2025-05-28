a9885553a5efdeeedf3c74eaac3e67fcfeecab97
Test <benjaminvincent1996@gmail.com>
2025-05-27 20:55:02 -0700

    Some basic practice.

---
+++ Creating_a_Document.py
@@ -0,0 +1,5 @@
+from docx import Documents
+
+doc = Documents()
+doc.add_paragraph("It works Benjamin loves her")
+doc.save("Benjamin")
---
+++ Word_Generation.py
@@ -0,0 +1,4 @@
+# Just some basic code to implement some various 6-bit words.
+# I should build a STRAVA_ACTIVITY_CORRECTOR
+for u in range(0, 256):
+    print(f"{u: 08b}")
